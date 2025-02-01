000eccb37695f6a45c1110322e7b50e02a6e81c8
imran <imran8122005@gmail.com>
2025-02-01 20:01:41 +0530

    fixed icon

--- src/main/java/currency/Main.java
+++ src/main/java/currency/Main.java
@@ -12,24 +12,34 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
+            // Debug: Print the resource path
+            System.out.println("Attempting to load icon from: " + getClass().getResource("/currency/images/logo.png"));
+            
+            // Try alternative resource loading
+            var iconStream = Main.class.getResourceAsStream("/currency/images/logo.png");
+            if (iconStream != null) {
+                Image icon = new Image(iconStream);
+                primaryStage.getIcons().add(icon);
+            } else {
+                System.err.println("Could not load icon - resource not found");
+            }
+            
             // Load the FXML file
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/currency/UI.fxml"));
             Parent root = loader.load();
 
             // Set the stage
             primaryStage.setTitle("Currency Converter");
-            primaryStage.setScene(new Scene(root, 400, 300));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setResizable(false);  // Optional: prevent window resizing
             primaryStage.show();
-
-            // Set the icon
-            Image icon = new Image(getClass().getResourceAsStream("/currency/images/logo.png"));
-            primaryStage.getIcons().add(icon);
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        launch(args); // Launch the JavaFX application
+        launch(args);
     }
 }
